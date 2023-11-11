45ab7f7a3e4b24d86b17f49766fb9aec9a7fa5bd
halfprice06 <danprce@gmail.com>
2023-11-10 18:41:38 -0600

    added users and authentication

--- main.py
+++ main.py
@@ -1,160 +1,168 @@
-
-from fastapi import FastAPI, HTTPException, Request, Form
-from pydantic import BaseModel
-from typing import List, Optional
-from uuid import uuid4, UUID
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from datetime import datetime, timedelta
+from fastapi import Depends, FastAPI, Form, HTTPException, Request, Response, status, Cookie
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.templating import Jinja2Templates
-import pickle
-import os
-import openai
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from typing import Optional
 
+import models  # Add this line
+
+from database import Base, SessionLocal, engine
+from models import create_todo, delete_todo, get_todo, update_todo, User, UserCreate, UserOut
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_user(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def verify_password(password: str, hashed_password: str):
+    return CryptContext().verify(password, hashed_password)
+
+def get_token_from_cookie(cookie: Optional[str] = Cookie(None)):
+    print(f"Cookie: {cookie}")  # Print the entire cookie
+    return cookie
+
+def get_current_user(token: str = Depends(get_token_from_cookie), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    if not token:
+        raise credentials_exception
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = models.TokenData(username=username)
+    except JWTError as e:
+        print(f"JWTError: {e}")  # Log the error message
+        raise credentials_exception
+    user = models.get_user(db, username=token_data.username)
+    print(f"User from database: {user}")  # Print the user from database
+    if user is None:
+        raise credentials_exception
+    return user
+
+pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
 
 @app.get("/", response_class=HTMLResponse)
-def read_root():
-    with open('static/index.html', 'r') as f:
-        content = f.read()
-    return HTMLResponse(content=content)
-
-class ToDoItem(BaseModel):
-    id: Optional[UUID] = None
-    content: str
-    completed: bool = False
-
-class Subtask(BaseModel):
-    description: str
-
-class SubtasksResponse(BaseModel):
-    subtasks: List[Subtask] = []
-
-if os.path.exists('todo_items.pkl'):
-    with open('todo_items.pkl', 'rb') as f:
-        todo_items = pickle.load(f)
-else:
-    todo_items = []
-
-if os.path.exists('completed_items.pkl'):
-    with open('completed_items.pkl', 'rb') as f:
-        completed_items = pickle.load(f)
-else:
-    completed_items = []
-
-def save_lists():
-    with open('todo_items.pkl', 'wb') as f:
-        pickle.dump(todo_items, f)
-    with open('completed_items.pkl', 'wb') as f:
-        pickle.dump(completed_items, f)
-
-@app.get("/todo/", response_class=HTMLResponse)
-def read_todo_items(request: Request):
-    return templates.TemplateResponse("todo.html", {"request": request, "items": todo_items})
-
-@app.get("/todo/completed", response_class=HTMLResponse)
-def read_completed_items(request: Request):
-    return templates.TemplateResponse("completed.html", {"request": request, "items": completed_items})
-
-@app.post("/todo/", response_class=HTMLResponse)
-def create_todo_item(request: Request, content: str = Form(...)):
-    item = ToDoItem(content=content, id=uuid4())
-    todo_items.append(item)
-    save_lists()
-    return templates.TemplateResponse("single_todo.html", {"request": request, "item": item})
-
-@app.put("/todo/{item_id}/complete", response_model=ToDoItem)
-def complete_todo_item(item_id: UUID):
-    for idx, item in enumerate(todo_items):
-        if item.id == item_id:
-            item.completed = True
-            completed_item = todo_items.pop(idx)
-            completed_items.append(completed_item)
-            save_lists()
-            return completed_item
-    raise HTTPException(status_code=404, detail="Item not found")
+def home(request: Request, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    todos = models.get_todos(db, current_user.id)
+    context = {
+        "request": request,
+        "todos": todos,
+        "title": "Home"
+    }
+    return templates.TemplateResponse("home.html", context)
 
 
-
-@app.get("/todo/{item_id}", response_model=ToDoItem)
-def read_todo_item(item_id: UUID):
-    for item in todo_items:
-        if item.id == item_id:
-            return item
-    raise HTTPException(status_code=404, detail="Item not found")
-
-@app.put("/todo/{item_id}", response_model=ToDoItem)
-def update_todo_item(item_id: UUID, item: ToDoItem):
-    for idx, db_item in enumerate(todo_items):
-        if db_item.id == item_id:
-            todo_items[idx] = item
-            item.id = item_id
-            save_lists()
-            return item
-    raise HTTPException(status_code=404, detail="Item not found")
+# In your routes, replace session_key with user_id
+@app.post("/add", response_class=HTMLResponse)
+def post_add(request: Request, content: str = Form(...), db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    todo = create_todo(db, content=content, user_id=current_user.id)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/item.html", context)
 
 
-@app.delete("/todo/{item_id}", response_model=ToDoItem)
-def delete_todo_item(item_id: UUID):
-    for idx, item in enumerate(todo_items):
-        if item.id == item_id:
-            todo_items.pop(idx)
-            save_lists()
-            return item
-    raise HTTPException(status_code=404, detail="Item not found")
+@app.get("/edit/{item_id}", response_class=HTMLResponse)
+def get_edit(request: Request, item_id: int, db: Session = Depends(get_db)):
+    todo = get_todo(db, item_id)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/form.html", context)
 
-@app.delete("/todo/completed/{item_id}", response_model=ToDoItem)
-def delete_completed_item(item_id: UUID):
-    for idx, item in enumerate(completed_items):
-        if item.id == item_id:
-            completed_items.pop(idx)
-            save_lists()
-            return item
-    raise HTTPException(status_code=404, detail="Item not found")
 
-@app.post("/autosubtask/", response_model=List[ToDoItem])
-async def autosubtask(content: str = Form(...)):
-    # Import OpenAI API key from environment
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+@app.put("/edit/{item_id}", response_class=HTMLResponse)
+def put_edit(request: Request, item_id: int, content: str = Form(...), db: Session = Depends(get_db)):
+    todo = update_todo(db, item_id, content)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/item.html", context)
 
-    # Send the task description to OpenAI's Chat completions endpoint
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": """ You are part of an ai assisted to-do app. You going to receive a 'to-do' task from a user, and then you are going to break this task down into subtasks. Return your response as json in the following format {
-                    "subtasks": [
-                        {
-                            "description": "Subtask 1 description"
-                        },
-                        {
-                            "description": "Subtask 2 description"
-                        },
-                        // More subtasks can be added here
-                    ]
-                }. Do not return anything other than json. Do not return an excessive amount of subtasks. This is for an attorney's timekeeping system, so use LEDES style entries."""},
-            {"role": "user", "content": content}
-        ]
+
+@app.delete("/delete/{item_id}", response_class=Response)
+def delete(item_id: int, db: Session = Depends(get_db)):
+    delete_todo(db, item_id)
+
+@app.post("/register", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.get("/login", response_class=HTMLResponse)
+def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    db_user = get_user(db, username)
+    if db_user is None or not verify_password(password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": username}, expires_delta=access_token_expires
     )
+    print(f"Access Token: {access_token}")  # Print the access token
+    response = RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie("access_token", access_token, httponly=True, samesite="None", secure="False")
+    return response
 
-    # Parse the JSON response to get the subtasks
-    subtasks_json = response.choices[0].message["content"]
+@app.get("/test-token")
+def test_token():
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": "testuser"}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token}
 
-    print(subtasks_json)
-    # Here you need to parse the subtasks_json into your expected format
-    subtasks_data = SubtasksResponse.parse_raw(subtasks_json)
-
-    # Add the subtasks to your todo list
-    new_subtasks = []
-    for subtask in subtasks_data.subtasks:
-        new_subtask = ToDoItem(content=subtask.description, id=uuid4())
-        todo_items.append(new_subtask)
-        new_subtasks.append(new_subtask)
-    
-    # Save the updated todo list
-    save_lists()
-    
-    # Return the newly created subtasks
-    return new_subtasks
+@app.get("/test-decode")
+def test_decode(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError as e:
+        return {"error": str(e)}
